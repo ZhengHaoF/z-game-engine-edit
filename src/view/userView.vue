dd024136a525779cb7514063031ae80e9569e970
ZhengHaoF <mynamezhf@gmail.com>
2022-12-28 10:05:16 +0800

    资源位置更改，增加测试用例

--- src/view/userView.vue
+++ src/view/userView.vue
@@ -16,6 +16,10 @@
         <a-menu-item key="1" @click="newScript">创建剧本</a-menu-item>
         <a-menu-item key="2" @click="inputScript">打开剧本</a-menu-item>
         <a-menu-item key="3" @click="outputScript">导出剧本</a-menu-item>
+        <a-menu-item key="4" @click="importSource">导入素材（测试）</a-menu-item>
+        <a-menu-item key="5" @click="getSource('/assets/backgroundImg/その他_プールA.png')">获取素材 （测试）</a-menu-item>
+        <a-menu-item key="6" @click="nodeTest">Node功能 （测试）</a-menu-item>
+
       </a-sub-menu>
       <a-sub-menu key="material">
         <template #icon>
@@ -23,15 +27,15 @@
         </template>
         <template #title>素材（material）</template>
         <a-menu-item key="1" @click="outputAll">导出所有</a-menu-item>
-<!--        <a-menu-item-group title="角色列表（roleList）">-->
-<!--          <a-menu-item key="setting:1">立绘（img）</a-menu-item>-->
-<!--          <a-menu-item key="setting:2">头图（headImg）</a-menu-item>-->
-<!--        </a-menu-item-group>-->
-<!--        <a-menu-item key="setting:3">背景列表（backgroundList）</a-menu-item>-->
-<!--        <a-menu-item-group title="所有音乐（musicList）">-->
-<!--          <a-menu-item key="setting:3">Option 3</a-menu-item>-->
-<!--          <a-menu-item key="setting:4">Option 4</a-menu-item>-->
-<!--        </a-menu-item-group>-->
+        <!--        <a-menu-item-group title="角色列表（roleList）">-->
+        <!--          <a-menu-item key="setting:1">立绘（img）</a-menu-item>-->
+        <!--          <a-menu-item key="setting:2">头图（headImg）</a-menu-item>-->
+        <!--        </a-menu-item-group>-->
+        <!--        <a-menu-item key="setting:3">背景列表（backgroundList）</a-menu-item>-->
+        <!--        <a-menu-item-group title="所有音乐（musicList）">-->
+        <!--          <a-menu-item key="setting:3">Option 3</a-menu-item>-->
+        <!--          <a-menu-item key="setting:4">Option 4</a-menu-item>-->
+        <!--        </a-menu-item-group>-->
       </a-sub-menu>
     </a-menu>
     <a-row style="padding-left: 10px;padding-right: 10px">
@@ -99,7 +103,7 @@
                     </template>
                   </template>
                   <template #expandedRowRender="{ record }">
-                    <a-list size="small" v-show="record.dialogue.content.length>0">
+                    <a-list v-show="record.dialogue.content.length>0" size="small">
                       <a-list-item>
                         <div style="width: 100%;display: flex;font-weight: bolder">
                           <div style="flex: 2">
@@ -146,22 +150,22 @@
             <a-tab-pane key="2" force-render tab="素材">
               <a-tabs>
                 <a-tab-pane key="3" tab="背景素材">
-                  <a-card hoverable style="width: 240px" v-for="item in chapterInfo['material']['backgroundList']">
+                  <a-card v-for="item in chapterInfo['material']['backgroundList']" hoverable style="width: 240px">
                     <template #cover>
-                      <img alt="example" :src="item['src']" />
+                      <img :src="item['src']" alt="example"/>
                     </template>
                     <a-card-meta :title="item['name']">
-                      <template #description>{{item['src']}}</template>
+                      <template #description>{{ item['src'] }}</template>
                     </a-card-meta>
                   </a-card>
                 </a-tab-pane>
                 <a-tab-pane key="4" tab="人物素材">
-                  <a-card hoverable style="width: 240px" v-for="item in chapterInfo['material']['roleList']">
+                  <a-card v-for="item in chapterInfo['material']['roleList']" hoverable style="width: 240px">
                     <template #cover>
-                      <img alt="example" :src="item['roleImg']" />
+                      <img :src="item['roleImg']" alt="example"/>
                     </template>
                     <a-card-meta :title="item['name']">
-                      <template #description>{{item['roleImg']}}</template>
+                      <template #description>{{ item['roleImg'] }}</template>
                     </a-card-meta>
                   </a-card>
                 </a-tab-pane>
@@ -170,10 +174,10 @@
                     <a-list>
                       <a-list-item v-for="item in  chapterInfo['material']['musicList']['role']">
                         <div style="flex: 10">
-                          <a-space>{{item['name']}}</a-space>
+                          <a-space>{{ item['name'] }}</a-space>
                         </div>
                         <div style="flex: 10">
-                          <a-space>{{item['src']}}</a-space>
+                          <a-space>{{ item['src'] }}</a-space>
                         </div>
                         <div style="flex: 4">
                           <a-button shape="circle" type="primary" @click="playAudio(item.roleMusic)">
@@ -192,7 +196,7 @@
         </div>
       </a-col>
     </a-row>
-    <a-modal wrap-class-name="full-modal" v-model:visible="editModal" title="编辑节点" width="100%" @ok="editModal=false">
+    <a-modal v-model:visible="editModal" title="编辑节点" width="100%" wrap-class-name="full-modal" @ok="editModal=false">
       <a-form>
         <a-form-item label="背景图片">
           <a-select v-model:value="nodeRow.background.name" :filter-option="filterOption" show-search>
@@ -298,16 +302,16 @@
 
 </template>
 <script lang="ts" setup>
-import {onMounted, Ref, ref} from "vue";
+import {computed, nextTick, onMounted, Ref, ref} from "vue";
 import * as localforage from "localforage";
 import {
-  MailOutlined,
+  DeleteOutlined,
   FormOutlined,
   HighlightOutlined,
-  VideoCameraAddOutlined,
+  MailOutlined,
   PlayCircleOutlined,
   PlusOutlined,
-  DeleteOutlined,
+  VideoCameraAddOutlined,
 } from '@ant-design/icons-vue';
 import {message} from 'ant-design-vue';
 import JSZip from "jszip";
@@ -453,24 +457,20 @@ const outputScript = async function () {
 /**
  * 添加节点
  */
-const addNode = function(){
+const addNode = function () {
   console.log(chapterInfo.value.node.push(
       {
-        "id":"",
-        "dialogue":{
-          "content":[
-
-          ]
+        "id": "",
+        "dialogue": {
+          "content": []
         },
-        "role":[
-
-        ],
-        "background":{
-          "name":""
+        "role": [],
+        "background": {
+          "name": ""
         },
-        "music":{
-          "backgroundMusic":{
-            "name":""
+        "music": {
+          "backgroundMusic": {
+            "name": ""
           }
         }
       }
@@ -581,19 +581,80 @@ const inputScript = function () {
     console.log(file.files[0])
     let reader = new FileReader();
     reader.readAsText(file.files[0]);//发起异步请求
-    reader.onload = function () {
+    reader.onload = function (e) {
       //读取完成后，数据保存在对象的result属性中
       try {
-        if (typeof this.result === "string") {
-          scriptRow.value = JSON.parse(this.result);
+        if (typeof e.target.result === "string") {
+          scriptRow.value = JSON.parse(e.target.result);
           message.success("导入成功")
-          sessionStorage.setItem("scriptRow", this.result)
           getChapterList();
         }
       } catch (e) {
         message.error("导入失败", e.message)
       }
     }
+  }
+}
+
+/**
+ * 导入全部素材
+ */
+const importSource = function () {
+  chapterInfo.value = {};
+  let file = document.createElement('input')
+  file.type = "file";
+  file.click();
+  file.onchange = function () {
+    let zip = new JSZip()
+    //同步读取压缩文件
+    zip.loadAsync(file.files[0]).then(() => {
+      for (let fileName of Object.keys(zip['files'])) {
+        if (!zip['files'][fileName].dir) {
+          //不是文件夹
+          //以二进制的新式存储到indexDB中去
+
+          if (
+              fileName.indexOf("jpg") !== -1 ||
+              fileName.indexOf("png") !== -1 ||
+              fileName.indexOf("bmp") !== -1 ||
+              fileName.indexOf("webp") !== -1
+          ) {
+            //对于图片采用base64编码
+            localforage.setItem(fileName, zip.file(fileName).async("blob"))
+          } else if (
+              fileName.indexOf("txt") !== -1 ||
+              fileName.indexOf("json") !== -1
+          ) {
+            //对于文本直接读取
+            localforage.setItem(fileName, zip.file(fileName).async("blob"))
+          } else {
+            //不识别的直接跳过
+            return;
+          }
+        } else {
+
+        }
+      }
+    })
+    localforage.getItem("scriptRow.json").then((res: any) => {
+      //读取剧本文件
+      let reader = new FileReader();
+      reader.readAsText(res);
+      reader.onload = function (e) {
+        //读取完成后，数据保存在对象的result属性中
+        try {
+          if (typeof e.target.result === "string") {
+            console.log(e.target.result)
+            scriptRow.value = JSON.parse(e.target.result);
+            message.success("导入成功")
+            getChapterList();
+          }
+        } catch (e) {
+          message.error("导入失败", e.message)
+        }
+      }
+    })
+    // console.log(getSource("scriptRow.json"))
   }
 }
 
@@ -650,6 +711,53 @@ const addDialogue = function () {
 const delDialogue = function (index) {
   nodeRow.value.dialogue.content.splice(index, 1)
 }
+
+/**
+ * 调用获取存储在indexDB中的资源
+ * @param sourceSrc
+ */
+const getSource = async function (sourceSrc) {
+  let sourceBlob = await localforage.getItem(sourceSrc)
+  let reader = new FileReader();
+  if (
+      sourceSrc.indexOf("jpg") !== -1 ||
+      sourceSrc.indexOf("png") !== -1 ||
+      sourceSrc.indexOf("bmp") !== -1 ||
+      sourceSrc.indexOf("webp") !== -1
+  ) {
+    //对于图片采用base64编码
+    reader.readAsDataURL(<Blob>sourceBlob)
+  } else if (
+      sourceSrc.indexOf("txt") !== -1 ||
+      sourceSrc.indexOf("json") !== -1
+  ) {
+    //对于文本直接读取
+    reader.readAsText(<Blob>sourceBlob);
+  } else {
+    //不识别的直接跳过
+    return;
+  }
+
+  reader.onload = function (e) {
+    console.log(e.target.result)
+    return e.target.result
+  }
+}
+
+/**
+ * node功能测试
+ */
+const nodeTest = function (){
+  let fs = nw.require('fs');
+  fs.readFile("package.json", 'utf8', function(err, txt) {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log(txt);
+  });
+}
+
 /**
  * 智能匹配
  */
